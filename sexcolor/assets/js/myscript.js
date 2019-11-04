782e875a67791ffbeb348147034dcf345e0ef568
vonaxl <vonaxlcorso@gmail.com>
2019-11-04 16:35:33 +0100

    done

--- sexcolor/assets/js/myscript.js
+++ sexcolor/assets/js/myscript.js
@@ -1,2 +1,15 @@
+// chiedi il nome 
+var nome=prompt("Come ti chiami?");
+// chiedi il suo sesso 
+var sesso=prompt("di che sesso sei? M/F");
 
-document.getElementById('n1out').innerHTML = n1;
+var nameOutput=document.getElementById("nomeOut");
+var sessoOutput=document.getElementById("sessoOut");
+
+nameOutput.innerHTML=nome;
+
+if (sesso=="M" || sesso == "m") {
+    nameOutput.style.color="lightblue";
+}else if (sesso=="F" || sesso == "f") {
+    nameOutput.style.color="pink";
+}
